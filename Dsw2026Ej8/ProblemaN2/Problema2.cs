58c3b26b6ada285b224dfd1c7e1de98f0ad9d2c9
NachoPaz <82242852+Nacho-Paz@users.noreply.github.com>
2026-05-03 10:07:15 -0300

    Problema 2

---
+++ Dsw2026Ej8/ProblemaN2/Problema2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Dsw2026Ej8.ProblemaN2
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+
+            var venta = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = quantity > 0 ? quantity * unitPrice : 0
+            };
+
+            return $"{venta.Code}-{venta.Description}-{venta.Total.ToString("C2", new CultureInfo("es-AR"))}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dsw2026Ej8.ProblemaN1;
+using Dsw2026Ej8.ProblemaN2;
 
 
 namespace Dsw2026Ej8
@@ -11,6 +12,9 @@ namespace Dsw2026Ej8
             /*ProductHelper p1 = new ProductHelper(1234, "Mouse", 60000);
             Console.WriteLine(p1.ObtenerEtiquetaProducto(p1.GetCode(), p1.GetDescription(), p1.GetPrice()));*/
 
+            //PROBLEMA 2
+            /*Problema2 problema2 = new Problema2();
+            Console.WriteLine(problema2.CrearResumenVenta(1234, "Mouse", 3, 60000));*/
 
         }
     }
